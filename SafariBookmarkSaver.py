1fa0762ec2a81fbb57d4615391d7de7c9ee47e2b
Andrew Godley <agdevlabs@gmail.com>
2019-12-18 21:16:34 +0000

    corrections

--- SafariBookmarkSaver.py
+++ SafariBookmarkSaver.py
@@ -246,14 +246,14 @@ def check_saved_bookmarks(in_attempts, out_succeed, out_failed):
 
 def check_site_and_save(toSave, full_string, out_attempted_args, url_list, full_file_path,to_save):
 	req = Request(toSave, headers=userAgent)
-		try:
-			request_code = urlopen(req).getcode()
-			if request_code == 200:
-				save_site_as_picture(toSave, full_string,out_attempted_args)
-				if to_save:
-					url_list[entry['URL']] = full_file_path
-		except:
-			pass
+	try:
+		request_code = urlopen(req).getcode()
+		if request_code == 200:
+			save_site_as_picture(toSave, full_string,out_attempted_args)
+			if to_save:
+				url_list[entry['URL']] = full_file_path
+	except:
+		pass
 
 def loop_and_save_bookmarks(in_bookmark_dict, out_all_store , out_attempted_store, out_attempted_args ):
 	url_list = {}
