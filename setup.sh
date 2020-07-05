fda0907fd49eec1456bfd0b29ef5cb5ba2e6d19b
Andrew <agdevlabs@gmail.com>
2020-07-05 21:01:44 +0100

    updated scripts

--- setup.sh
+++ setup.sh
@@ -2,10 +2,16 @@ echo $SHELL
 
 if [ $SHELL = '/bin/zsh' ]; then
 
-	echo zsh
+	echo 'setting up scripts for zsh shell'
+	echo "alias randomSelector=\"python $(pwd)/SafariBookmarkSaver.py -S -n 6 -r 70 -t ~/Desktop\"" >> ~/.zshrc
+	echo "alias bookmarkSaver=\"python $(pwd)/SafariBookmarkSaver.py -j -t ./\"" >> ~/.zshrc
+	echo "alias bookmarkSaverPDF=\"python $(pwd)/SafariBookmarkSaver.py -j -t ./\"" >> ~/.zshrc
 
 elif [$SHELL = '/bin/bash' ]; then
 
-	echo bash
+	echo 'setting up scripts for bash shell'
+	echo "alias bookmarkSaver=\"python $(pwd)/SafariBookmarkSaver.py -S -n 6 -r 70 -t ~/Desktop\"" >> ~/.bashrc
+	echo "alias bookmarkSaver=\"python $(pwd)/SafariBookmarkSaver.py -j -t ./\"" >> ~/.bashrc
+	echo "alias bookmarkSaverPDF=\"python $(pwd)/SafariBookmarkSaver.py -j -t ./\"" >> ~/.bashrc
 
 fi
